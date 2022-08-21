ff4ab1b3cfc5886f8f812a5e1ad0aecfebc4a415
k1shan <93940049+k1shan-k@users.noreply.github.com>
2022-08-21 22:46:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from datetime import datetime
+from datetime import datetime 
 app = Flask(__name__)
 
 @app.route("/")
 
 def hello_world():
-    return "Hello world"
+    return "Hello World"
 
 @app.route("/time")
 
