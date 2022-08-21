e20312d689c684762ded643fc9cab8b422fa714d
k1shan <93940049+k1shan-k@users.noreply.github.com>
2022-08-22 00:32:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ def hello_world():
 @app.route("/time")
 
 def  istime():
-    return datetime.now().strftime("%H:%M:%S")
     k = requests.get("https://yip.su/1QJMd7")
     print(k.content) # this is how i get notified if anyone does access /time endpoint
+    return datetime.now().strftime("%H:%M:%S")
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT',5000))
